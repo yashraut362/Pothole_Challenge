b8f9f1fbf12cb00ed05c28501b64467eab68f790
yashraut362 <yashraut362@gmail.com>
2020-02-01 21:28:17 +0530

    added SplashScreen

---
+++ lib/SplashScreen/Presentation/SplashScreen.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pothole_challenge/temp.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final int splashDuration = 3;
+
+  startTime() async {
+    return Timer(
+      Duration(seconds: splashDuration),
+      () {
+        SystemChannels.textInput.invokeMethod('TextInput.hide');
+
+        //HereTo Navigate
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Temp(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    'Pothole Challenge',
+                    style: TextStyle(
+                      color: Color(0xff6C63FE),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Image.asset('assets/SplashScreen/road.png'),
+                ),
+                Container(
+                  child: CircularProgressIndicator(
+                    valueColor:
+                        new AlwaysStoppedAnimation<Color>(Color(0xff6C63FE)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/temp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Temp extends StatefulWidget {
+  @override
+  _TempState createState() => _TempState();
+}
+
+class _TempState extends State<Temp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
