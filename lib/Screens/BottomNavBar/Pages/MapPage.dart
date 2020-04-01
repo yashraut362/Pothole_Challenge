4b4f5436dfc322920084a994e1f65411fc2967bb
yashraut362 <yashraut362@gmail.com>
2020-04-01 23:34:10 +0530

    added map

--- lib/Screens/BottomNavBar/Pages/MapPage.dart
+++ lib/Screens/BottomNavBar/Pages/MapPage.dart
@@ -1,24 +1,56 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
+  MapPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
   _MapPageState createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
+  GoogleMapController _controller;
+
+  final CameraPosition _initialPosition =
+      CameraPosition(target: LatLng(24.903623, 67.198367));
+
+  final List<Marker> markers = [];
+
+  addMarker(cordinate) {
+    int id = Random().nextInt(100);
+
+    setState(() {
+      markers
+          .add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Center(
-              child: Text('Map Here'),
-            ),
-          ],
-        ),
+      body: GoogleMap(
+        initialCameraPosition: _initialPosition,
+        mapType: MapType.normal,
+        onMapCreated: (controller) {
+          setState(() {
+            _controller = controller;
+          });
+        },
+        markers: markers.toSet(),
+        onTap: (cordinate) {
+          _controller.animateCamera(CameraUpdate.newLatLng(cordinate));
+          addMarker(cordinate);
+        },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _controller.animateCamera(CameraUpdate.zoomOut());
+        },
+        child: Icon(Icons.zoom_out),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
