11d5e8fb2a2fa2bb2366d4c17142dbb38f221d2b
yashraut362 <yashraut362@gmail.com>
2020-04-02 15:36:16 +0530

    added menu in newcomplaint

--- lib/Screens/HomeMenuPages/Presentation/NewComplaint.dart
+++ lib/Screens/HomeMenuPages/Presentation/NewComplaint.dart
@@ -12,7 +12,65 @@ class _NewComplaintState extends State<NewComplaint> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text("data"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              OutlineButton(
+                splashColor: Color(0xff4d80e4),
+                focusColor: Color(0xff4d80e4),
+                child: new Text(
+                  "Capture through camera",
+                  style: TextStyle(
+                    color: Color(0xff4d80e4),
+                  ),
+                ),
+                onPressed: null,
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+              ),
+              Divider(
+                color: Color(0xff2e279d),
+                height: 20.0,
+                endIndent: 150,
+                indent: 150,
+                thickness: 0.5,
+              ),
+              OutlineButton(
+                splashColor: Color(0xff4d80e4),
+                focusColor: Color(0xff4d80e4),
+                child: new Text(
+                  "Upload Through Gallery",
+                  style: TextStyle(
+                    color: Color(0xff4d80e4),
+                  ),
+                ),
+                onPressed: null,
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+              ),
+              Divider(
+                color: Color(0xff2e279d),
+                height: 20.0,
+                endIndent: 150,
+                indent: 150,
+                thickness: 0.5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FloatingActionButton(
+                    onPressed: null,
+                    backgroundColor: Color(0xff4d80e4),
+                    child: Icon(Icons.question_answer),
+                  ),
+                  SizedBox(width: 20),
+                  Text("How to Report"),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/BottomNavBar/cards.dart
+++ lib/widgets/BottomNavBar/cards.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pothole/Screens/BottomNavBar/Pages/ComplaintPage.dart';
+import '../../Routes/routeGenerator.dart';
+import '../../Screens/HomeMenuPages/Presentation/NewComplaint.dart';
 
 class Cards extends StatefulWidget {
   @override
@@ -15,37 +17,54 @@ class _CardsState extends State<Cards> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Container(
-              color: Color(0xff4d80e4),
-              child: const ListTile(
-                leading: Icon(Icons.add_location),
-                title: Text('Report new complaint'),
-                subtitle: Text('You can add new complaint by tapping here'),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NewComplaint()),
+                );
+              },
+              child: Container(
+                color: Color(0xff4d80e4),
+                child: const ListTile(
+                  leading: Icon(Icons.add_location),
+                  title: Text('Report new complaint'),
+                  subtitle: Text('You can add new complaint by tapping here'),
+                ),
               ),
             ),
-            Container(
-              color: Color(0xff4d80e4),
-              child: const ListTile(
-                leading: Icon(Icons.time_to_leave),
-                title: Text('Status of complaint'),
-                subtitle:
-                    Text('Here you can see status About Your New Complaint'),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                color: Color(0xff4d80e4),
+                child: const ListTile(
+                  leading: Icon(Icons.time_to_leave),
+                  title: Text('Status of complaint'),
+                  subtitle:
+                      Text('Here you can see status About Your New Complaint'),
+                ),
               ),
             ),
-            Container(
-              color: Color(0xff4d80e4),
-              child: const ListTile(
-                leading: Icon(Icons.near_me),
-                title: Text('Nearby Complaints'),
-                subtitle: Text('Here You can see all nearby complaint'),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                color: Color(0xff4d80e4),
+                child: const ListTile(
+                  leading: Icon(Icons.near_me),
+                  title: Text('Nearby Complaints'),
+                  subtitle: Text('Here You can see all nearby complaint'),
+                ),
               ),
             ),
-            Container(
-              color: Color(0xff4d80e4),
-              child: const ListTile(
-                leading: Icon(Icons.check_box_outline_blank),
-                title: Text('FAQ'),
-                subtitle: Text('All Frequently Asked Question are here '),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                color: Color(0xff4d80e4),
+                child: const ListTile(
+                  leading: Icon(Icons.check_box_outline_blank),
+                  title: Text('FAQ'),
+                  subtitle: Text('All Frequently Asked Question are here '),
+                ),
               ),
             ),
           ],
