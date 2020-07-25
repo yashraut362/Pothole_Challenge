fd4747185de3a73a92261d8cba258ce19bd139b1
yashraut362 <yashraut362@gmail.com>
2020-07-25 19:26:48 +0530

    see pothole option done

--- lib/Screens/StatusAllScreen/services/DatabaseStatus.dart
+++ lib/Screens/StatusAllScreen/services/DatabaseStatus.dart
@@ -1,14 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
 getStatusList() async {
   return await Firestore.instance.collection('reports').getDocuments();
 }
 
-
 navigateme(String lat, String long) async {
-String url = "https://www.google.com/maps?q=" + lat + "," + long;
+  String url = "https://www.google.com/maps?q=" + lat + "," + long;
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+seepothole(String downloadurl) async {
+  String url = downloadurl;
   if (await canLaunch(url)) {
     await launch(url);
   } else {
